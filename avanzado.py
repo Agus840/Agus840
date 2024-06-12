825707cc7df470cbfeb7dd52772799815ec4d7b2
Agus840 <145867195+Agus840@users.noreply.github.com>
2024-06-12 15:36:20 -0300

    Veterinario.py

--- avanzado.py
+++ avanzado.py
@@ -1,24 +1,40 @@
 def Datos():
-	print("bienvenido a la veterinaria, porfavor ingrese los datos solicitados o ingrese 000 para terminar")
-	completos=[]
-	incompletos=[] 
-	nombre=input("ingrese su nombre: ")
-	while nombre!="000":
+	print("bienvenido a la veterinaria, ¿desea crear una lista de datos? Si para comenzar y No para finalizar")
+	lista=[]
+	inicio=input("ingrese si o no: ").upper()
+	while inicio!="NO":
+		nombre=input("ingrese su nombre: ")
 		apellido=input("ingrese su apellido: ")
 		dni=input("ingrese su DNI: ")
 		dueño=input("¿es el dueño del animal? (Si o No): ")
 		nom_ani=input("ingrese el nombre de la mascota: ")
 		edad=input("ingrese la edad de la mascota: ")
-		print("Porfavor siga ingresando los datos solicitados o ingrese 000 para terminar")
-		nombre=input("ingrese su nombre: ")
-		if (nombre==""or apellido==""or dni==""or dueño== "" or nom_ani=="" or edad== ""):
-			Dat_inc=nombre,apellido,dni,dueño,nom_ani,edad
-			incompletos.append(Dat_inc)
+		vacunas=input("¿cuantas vacunas tiene?: ")
+		inicio=input("¿desea crear otra lista de datos? Si para continuar y No para finalizar: ").upper()
+		datos= nombre,apellido,dni,dueño,nom_ani,edad,vacunas
+		lista.append(datos)
+	return lista 
+
+def separar_datos(lista):
+	completos=[]
+	incompletos=[]
+	for elemento in lista:
+		if (elemento[0]==""or elemento[1]==""or elemento[2]==""or elemento[3]== "" or elemento[4]=="" or elemento[5]== ""or elemento[6]== ""):
+			incompletos.append(elemento)
 		else:
-			Dat_com=nombre,apellido,dni,dueño,nom_ani,edad
-			completos.append(Dat_com)
-	return completos,incompletos
+			completos.append(elemento)
+	return [completos,incompletos]
 Dat=Datos()
-print(Dat[0])
-print("-----------------------")
-print(Dat[1])
+listas= separar_datos(Dat)
+print("completos ",listas[0])
+print("--------------------------")
+print("incompletos ",listas[1])
+
+def edad_acendente(lista):
+	print("lista acendente de edad de los animales")
+	for elemento in lista:
+		lista.sort()
+		print("________________________________________")
+		print(" Nombre del dueño: ",elemento[0]," Dni: ",elemento[2]," nombre del animal: ",elemento[4]," edad: ",elemento[5])
+		print("________________________________________")
+edad=edad_acendente(Dat)
